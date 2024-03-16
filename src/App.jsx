1f6e3269e831f78f40c4028e1088d58c9ca470a0
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-03-16 11:05:26 +0530

    configured tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-6xl text-red-500">HELLO WORLD!!!!!</div>;
+  return (
+    <div className="text-6xl text-red-500">
+      <h1 className="font-orbitron font-extrabold text-xl">Hello world</h1>
+      <h1 className="font-baumans text-xl">Hello world</h1>
+      <h1 className="font-saira text-xl">Hello world</h1>
+      <h1 className="font-poller text-xl">Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
